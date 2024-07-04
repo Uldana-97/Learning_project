d60407c695da55c4dffee3ec3baf395de0717adf
Uldana-97 <yessimovauldana@gmail.com>
2024-07-04 23:41:24 +0500

    New branch

---
+++ android/app/src/main/kotlin/com/cryptogirrls/crypto_coins_list/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.cryptogirrls.crypto_coins_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/features/crypto_list/view/crypto_list_screen.dart
@@ -0,0 +1,51 @@
+import 'package:crypto_coins_list/features/crypto_list/widgets/widgets.dart';
+import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins.dart';
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+
+class CryptoListScreen extends StatefulWidget {
+  const CryptoListScreen({
+    super.key,
+  });
+
+  @override
+  State<CryptoListScreen> createState() => _CryptoListScreenState();
+}
+
+class _CryptoListScreenState extends State<CryptoListScreen> {
+  List<CryptoCoin>? _cryptoCoinsList;
+
+  @override
+  void initState() {
+    _loadCryptoCoins();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('CryptoCurrenciesList'),
+      ),
+      body: (_cryptoCoinsList == null) 
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.separated(
+              padding: const EdgeInsets.only(top: 14),
+              itemCount: _cryptoCoinsList!.length,
+              separatorBuilder: (context, index) => const Divider(),
+              itemBuilder: (context, i) {
+                final coin = _cryptoCoinsList![i];
+                return CryptoCoinTile(coin: coin);
+              },
+            ),
+    );
+  }
+
+  Future<void> _loadCryptoCoins() async { 
+    _cryptoCoinsList = await GetIt.I<AbstractCoinsRepository>().getCoinsList();
+    setState(() {});
+  }
+}
+
+
